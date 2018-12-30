1667521181b8e1de2387283e9d410481a7c69089
Javier Machin <javier_machin_acosta@hotmail.com>
2018-12-30 13:41:10 +0000

    Add describe blocks

--- server.test.js
+++ server.test.js
@@ -3,34 +3,42 @@ const expect = require('expect');
 
 const app = require('./server').app;
 
-// Assert get root response is correct
-// We pass done as it will be an async test
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
+describe('Server', () => {
+
+  describe('GET "/"', () => {
+    // Assert get root ('/') response is correct
+    // We pass done as it will be an async test
+    it('should return hello world response', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            error: 'Page not found.'
+          });
+        })
+        .end(done);
+    });
+  })
+  
+  describe('GET "/users"', () => {
+    // Assert get '/users' response is correct
+    // We pass done as it will be an async test
+    it('should return an array of users including my name', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+          expect(res.body).toInclude({
+            name: 'Javi',
+            age: 100
+          })
+        })
+        .end(done);
+    });
+  });
 });
 
-
-// 
-it('should return an array of users including my name', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Javi',
-        age: 100
-      })
-    })
-    .end(done);
-});
 
 // We pass done as it will be an async test
 // it('should return hello world response', (done) => {
